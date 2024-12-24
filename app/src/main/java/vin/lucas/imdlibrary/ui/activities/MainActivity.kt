197ab8c21d38a2812bd6a12c318ae948de899520
Lucas Vinicius <lucas.vinicius@picpay.com>
2024-12-24 14:18:54 -0300

    Initial commit

---
+++ app/src/main/java/vin/lucas/imdlibrary/ui/activities/MainActivity.kt
@@ -0,0 +1,36 @@
+package vin.lucas.imdlibrary.ui.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import vin.lucas.imdlibrary.ui.theme.IMDLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IMDLibraryTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
